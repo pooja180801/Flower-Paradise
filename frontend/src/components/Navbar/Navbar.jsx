182b849fff7f610a35e08eab2eef71c6571b05ec
poojak <139669968+pooja180801@users.noreply.github.com>
2024-06-10 12:25:27 +0530

    Reapply "added navbar"
    This reverts commit ba2d82e6bb6cc4bbb5b40f3e7852cf9bc4bb42ed.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import logo from '../../assets/images/logo.png'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Navbar = () => {
+
+    const [menu,setMenu]=useState("home");
+
+
   return (
     <div className='navbar'>
-      
+      <img src={logo} alt="" className="logo" />
+      <ul className='navbar-menu'>
+        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
+        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
+        <li onClick={()=>setMenu("contact us")} className={menu==="contact us"?"active":""}>contact us</li>
+      </ul>
+      <div className='navbar-right'>
+      <SearchIcon/>
+          <div className="navbar-cart-icon">
+            <ShoppingCartIcon/>
+            <div className="dot"></div>
+          </div>
+          <button>Sign In</button>
+
+      </div>
     </div>
   )
 }
