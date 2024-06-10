52c9f6ac4b38172a1a1b07119f675b3ecdcd0eb5
poojak <139669968+pooja180801@users.noreply.github.com>
2024-06-10 12:18:04 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home/Home';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Cart from './pages/Cart/Cart';
 
 function App() {
   return (
     <div className="app">
       <Navbar/>   
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>} />
+      </Routes>
     </div>
   );
 }
