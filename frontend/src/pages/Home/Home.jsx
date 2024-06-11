efc23279aad19b3be7697a8849377b365bb57e08
poojak <139669968+pooja180801@users.noreply.github.com>
2024-06-11 11:53:07 +0530

    updated home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
+import ViewCollections from '../../components/viewBouquets/ViewCollections'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <ViewCollections/>
     </div>
   )
 }
