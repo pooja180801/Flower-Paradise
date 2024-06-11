1466fcb8fe3964189dfa9a04766ad8091fc54cba
poojak <139669968+pooja180801@users.noreply.github.com>
2024-06-11 11:52:19 +0530

    updated hedaer component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
 
 const Header = () => {
   return (
@@ -7,10 +7,10 @@ const Header = () => {
       <div className="header-content">
         <h2>Get Your Dream Flower Bouquet Delivered</h2>
         <p>Browse our exquisite collection of bouquets and order the one that speaks to you. With just a few clicks, you can bring the beauty of fresh flowers into your home or send a heartfelt gift to someone special.</p>
-        <button>View menu</button>
+        <button>View Menu</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
