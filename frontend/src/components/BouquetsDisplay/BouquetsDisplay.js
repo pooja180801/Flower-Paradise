de7eff3d8fd3ec92e50fb7a8a8363a8beae036c1
poojak <139669968+pooja180801@users.noreply.github.com>
2024-06-18 16:56:39 +0530

    added bouquetdisplay

---
+++ frontend/src/components/BouquetsDisplay/BouquetsDisplay.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react'
+import './BouquetsDisplay.css'
+import { StoreContext } from '../context/StoreContext'
+import BouquetItem from '../BouquetItem/BouquetItem'
+
+const BouquetsDisplay = ({category}) => {
+
+    const {bouquets_lists}=useContext(StoreContext)
+  return (
+    <div className='bouquet-display' id='bouquet-display'>
+        <h2>View Bouquets</h2>
+        <div className="bouquets-display-list">
+            {bouquets_lists.map((item,index)=>{
+                    return <BouquetItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+                })}
+        </div>
+    </div>
+  )
+}
+
+export default BouquetsDisplay
