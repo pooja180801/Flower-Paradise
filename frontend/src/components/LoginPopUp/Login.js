1148a2e9ca3ec9e62b144153c3d49217bdc49091
poojak <139669968+pooja180801@users.noreply.github.com>
2024-07-04 20:01:22 +0530

    uploaded login

---
+++ frontend/src/components/LoginPopUp/Login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import './Login.css'
+import { IoIosCloseCircleOutline } from "react-icons/io";
+
+const Login = ({setShowLogin}) => {
+
+    const [currentState,setCurrentState]=useState("Login")
+
+  return (
+    <div className='login-popup'>
+      <form className='login-container'>
+        <div className="login-header">
+            <h2>{currentState}</h2>
+            <IoIosCloseCircleOutline onClick={()=>setShowLogin(false)} className='cross-icon'/>
+        </div>
+        <div className="login-input">
+            {currentState==="Login"?<></>:<input type="text" placeholder='Username' />}
+            <input type="email" placeholder='Email' />
+            <input type="password" placeholder='Password' />
+        </div>
+        <button>{currentState==="Sign Up"?"Register":"Login"}</button>
+        <div className="login-condition">
+            <input type="checkbox" required/>
+            <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+        {currentState==="Login"
+        ?<p>Already have an account?<span onClick={()=>setCurrentState("Sign Up")}>Login Here</span></p>
+        :<p>Create a new account?<span onClick={()=>setCurrentState("Login")}>Click Here</span></p>
+    }
+        
+        
+      </form>
+    </div>
+  )
+}
+
+export default Login
