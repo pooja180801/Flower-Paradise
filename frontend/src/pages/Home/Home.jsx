9562c2b899db9a2b762ae3cc90abd4d33d3f175e
poojak <139669968+pooja180801@users.noreply.github.com>
2024-07-04 20:01:56 +0530

    updated home.js

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ViewCollections from '../../components/viewBouquets/ViewCollections'
+import BouquetsDisplay from '../../components/BouquetsDisplay/BouquetsDisplay'
 
 const Home = () => {
+
+  const [category,setCategory]=useState("All")
   return (
     <div>
       <Header/>
-      <ViewCollections/>
+      <ViewCollections category={category} setCategory={setCategory}/>
+      <BouquetsDisplay category={category}/>
     </div>
   )
 }
