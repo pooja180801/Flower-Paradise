613235b176d43a7c6ed36629d5728904d8118d76
poojak <139669968+pooja180801@users.noreply.github.com>
2024-07-04 19:59:39 +0530

    updated header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
   return (
     <div className='header'>
-      <div className="header-content">
+      <div className='header-content'>
         <h2>Get Your Dream Flower Bouquet Delivered</h2>
         <p>Browse our exquisite collection of bouquets and order the one that speaks to you. With just a few clicks, you can bring the beauty of fresh flowers into your home or send a heartfelt gift to someone special.</p>
         <button>View Menu</button>
