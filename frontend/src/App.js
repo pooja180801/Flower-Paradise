74f33d7b608222377faf81401deaaf223626a7e4
poojak <139669968+pooja180801@users.noreply.github.com>
2024-07-04 19:59:53 +0530

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,26 @@ import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home/Home';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Cart from './pages/Cart/Cart';
+import Footer from './components/Footer/Footer';
+import { useState } from 'react';
+import Login from './components/LoginPopUp/Login';
 
 function App() {
+
+  const [showLogin,setShowLogin]=useState(false)
   return (
+    <>
+    {showLogin?<Login setShowLogin={setShowLogin}/>:<></>}
     <div className="app">
-      <Navbar/>   
+      <Navbar setShowLogin={setShowLogin}/>   
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/cart' element={<Cart/>} />
         <Route path='/order' element={<PlaceOrder/>} />
       </Routes>
     </div>
+    <Footer/>
+    </>
   );
 }
 
