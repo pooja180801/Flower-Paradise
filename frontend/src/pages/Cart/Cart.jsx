f7fad90d0f204204f97dad362466307012586df5
poojak <139669968+pooja180801@users.noreply.github.com>
2024-07-05 16:27:18 +0530

    uploaded cartpage

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../components/context/StoreContext'
+import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
+
+  const {cartItems,bouquets_lists,removeFromCart,getTotalCartAmount}=useContext(StoreContext)
+
+  const navigate=useNavigate();
+
   return (
-    <div>
-      
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {bouquets_lists.map((item,index)=>{
+          if(cartItems[item.id]>0){
+            return(
+              <div>
+              <div className='cart-items-title cart-items-item'>
+                <img src={require(`../../assets/images/${item.image}`)} alt="" />
+                <p>{item.name}</p>
+                <p>Rs.{item.price}</p>
+                <p>{cartItems[item.id]}</p>
+                <p>Rs.{item.price*cartItems[item.id]}</p>
+                <p onClick={()=>removeFromCart(item.id)} className='x'>X</p>
+              </div>
+              <hr />
+              </div>
+            )
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>Rs.{getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>Rs.{getTotalCartAmount()===0?0:150}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>Rs.{getTotalCartAmount()===0?0:getTotalCartAmount()+150}</b>
+            </div>
+            <div className='checkout-button'>
+            <button onClick={()=>navigate('/order')}>PROCEED TO CHECK OUT</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
