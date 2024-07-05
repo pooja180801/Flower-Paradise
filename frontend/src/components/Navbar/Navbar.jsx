94510ab7b848e570dfbdb6e8e849c0c06dfa3ae5
poojak <139669968+pooja180801@users.noreply.github.com>
2024-07-05 16:27:06 +0530

    updated navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,30 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/images/logo.png'
 import { FaSearch } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { StoreContext } from '../context/StoreContext';
 
 
 const Navbar = ({setShowLogin}) => {
 
     const [menu,setMenu]=useState("home");
-
+    const {getTotalCartAmount}=useContext(StoreContext)
 
   return (
     <div className='navbar'>
-      <img src={logo} alt="" className="logo" />
+      <Link to='/'><img src={logo} alt="" className="logo" /></Link>
       <ul className='navbar-menu'>
-        <Link onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
-        <a href='#view-collections' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
+        <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+        <a href='#view-collections' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>collections</a>
         <a href='#footer' onClick={()=>setMenu("contact us")} className={menu==="contact us"?"active":""}>contact us</a>
       </ul>
       <div className='navbar-right'>
       <FaSearch/>
           <div className="navbar-cart-icon">
-            <FaShoppingCart/>
-            <div className="dot"></div>
+            <Link to='/cart'><FaShoppingCart/></Link>
+            <div className={getTotalCartAmount()===0?"":"dot"}></div>
           </div>
           <button onClick={()=>setShowLogin(true)}>Sign In</button>
 
