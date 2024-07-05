3adc0a78a8e9a46c48e426297f57c599fcbf0766
poojak <139669968+pooja180801@users.noreply.github.com>
2024-07-05 18:28:25 +0530

    uploaded server.js

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import bouquetRouter from "./routes/bouquetRoute.js"
+
+
+//app config
+const app=express()
+const port=4000
+
+//middleware
+//the req from frontend to backend that will parsed using this json
+app.use(express.json())
+//to access the backend from any frontend
+app.use(cors())
+
+
+//db connection
+connectDB();
+
+//api endpoint
+app.use("/api/bouquet",bouquetRouter)
+
+
+app.get("/",(req,res)=>{
+    res.send("API working")
+})
+
+app.listen(port,()=>{
+console.log(`Server started on https://localhost:4000`)
+})
+
