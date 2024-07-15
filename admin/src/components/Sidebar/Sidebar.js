c5f9189ad8d56dffbfc0694ba4a4a18adbd946fc
poojak <139669968+pooja180801@users.noreply.github.com>
2024-07-15 19:09:18 +0530

    added sidebar for admin

--- admin/src/components/Sidebar/Sidebar.js
+++ admin/src/components/Sidebar/Sidebar.js
@@ -2,23 +2,24 @@ import React from 'react'
 import './Sidebar.css'
 import { IoAdd } from "react-icons/io5";
 import { FaClipboardCheck } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
         <div className="sidebar-options">
-            <div className="sidebar-option">
-                <IoAdd/>
+            <NavLink to='/add' className="sidebar-option">
+                <IoAdd className='icon'/>
                 <p>Add Items</p>
-            </div>
-            <div className="sidebar-options">
-                <FaClipboardCheck/>
+            </NavLink>
+            <NavLink to='/list' className="sidebar-option">
+                <FaClipboardCheck className='icon'/>
                 <p>List Items</p>
-            </div>
-            <div className="sidebar-options">
-                <FaClipboardCheck/>
+            </NavLink>
+            <NavLink to='/orders' className="sidebar-option">
+                <FaClipboardCheck className='icon'/>
                 <p>Orders</p>
-            </div>
+            </NavLink>
         </div>
       
     </div>
