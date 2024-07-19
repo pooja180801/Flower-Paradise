07eee177e81a995539d2f4d6ca1d328131d77045
poojak <139669968+pooja180801@users.noreply.github.com>
2024-07-19 18:17:42 +0530

    updated db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,10 @@
-import mangoose from 'mongoose'
+import mongoose from 'mongoose';
 
-export const connectDB=async()=>{
-    await mangoose.connect('mongodb+srv://flowerparadise:18082001pk@cluster0.uwqabev.mongodb.net/flower-paradise')
-    .then(()=>console.log("db connected"))
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://flowerparadise:18082001pk@cluster0.uwqabev.mongodb.net/flower-paradise');
+    console.log("DB connected");
+  } catch (error) {
+    console.error("DB connection error:", error);
+  }
+};
