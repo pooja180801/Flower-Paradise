7fc995b1ba45a8f58789c56f599eb9f625411284
poojak <139669968+pooja180801@users.noreply.github.com>
2024-07-19 09:39:40 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import { connectDB } from "./config/db.js"
 import bouquetRouter from "./routes/bouquetRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 //app config
@@ -24,6 +26,8 @@ connectDB();
 app.use("/api/bouquet",bouquetRouter)
 app.use("/images",express.static('uploads'))
 app.use('/api/user',userRouter)
+app.use('/api/cart',cartRouter)
+app.use('api/order',orderRouter)
 
 
 app.get("/",(req,res)=>{
