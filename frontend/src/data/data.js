89484c5c5f5b99293ea2d5edcfa395b6c07ac52d
poojak <139669968+pooja180801@users.noreply.github.com>
2025-04-07 09:08:22 +0530

    inseeted dummy data

---
+++ frontend/src/data/data.js
@@ -0,0 +1,42 @@
+export const collections_list = [
+    {
+        collection_name: "Rose Arrangements",
+        img_src: "collection1.png",
+        description: "Celebrate love with our stunning anniversary rose collection."
+    },
+    {
+        collection_name: "Birthday Collections",
+        img_src: "collection2.png",
+        description: "A vibrant collection inspired by the beauty of spring blossoms."
+    },
+    {
+        collection_name: "Love & Romance Collection",
+        img_src: "collection3.png",
+        description: "Embrace the warmth of summer with our breezy collection."
+    },
+    {
+        collection_name: "Anniversary Collection",
+        img_src: "collection4.png",
+        description: "Rich and warm, perfect for the fall season."
+    },
+    {
+        collection_name: "Congraulations Collection",
+        img_src: "collection5.png",
+        description: "Cool and enchanting, capturing the essence of winter."
+    },
+    {
+        collection_name: "Thank You Collection",
+        img_src: "collection6.png",
+        description: "Make birthdays special with our unique surprise collection."
+    },
+    {
+        collection_name: "Sympathy Collection",
+        img_src: "collection7.png",
+        description: "Spread joy and cheer with our festive holiday collection."
+    },
+    {
+        collection_name: "Get well Collection",
+        img_src: "collection8.png",
+        description: "Ring in the new year with a touch of sparkle and glamour."
+    }
+];
---
+++ frontend/src/data/flowerList.js
@@ -0,0 +1,121 @@
+export const bouquets_lists=[
+    {
+        id:"1",
+        name:"Cool Blue Fresh Rose Bouquet",
+        image:"bouquet11.jpg",
+        description:"The Cool Blue Fresh Rose Bouquet features six white roses, sprayed blue, elegantly wrapped in a black wrapping.",
+        category:"Rose Arrangements",
+        price:"3000"
+    },
+    {
+        id:"2",
+        name:"Tender Rose Posy with Love Cookie",
+        image:"bouquet12.jpg",
+        description:"The Tender Rose Posy with Love Cookie includes one red rose, two pink roses, two stems of chrysanthemums, and a love-stamped butter cookie, all wrapped in a pink letter wrapping.",
+        category:"Rose Arrangements",
+        price:"2550"
+    },
+    {
+        id:"3",
+        name:"Caribbean Twist Fresh Flower Bouquet",
+        image:"bouquet21.jpg",
+        description:"  ",
+        category:"Birthday Collections",
+        price:"4385"
+    },
+    {
+        id:"4",
+        name:"Bouquet of 25 Red Roses",
+        image:"bouquet31.jpg",
+        description:"",
+        category:"Love & Romance Collection",
+        price:"9790"
+    },
+    {
+        id:"5",
+        name:"100 Eternal Kisses",
+        image:"bouquet41.jpg",
+        description:"",
+        category:"Anniversary Collection",
+        price:"38480"
+    },
+    {
+        id:"6",
+        name:"Blossom Dreams Bouquet",
+        image:"bouquet14.jpg",
+        description:"Gift a moment of pure joy with our Blossom Dreams Bouquet—a dazzling ensemble of 20 fresh red roses and golden rods, beautifully wrapped in golden heart sheets.",
+        category:"Rose Arrangements",
+        price:"8170"
+    },
+    {
+        id:"7",
+        name:"Say I Love You Fresh Red Roses",
+        image:"bouquet13.jpg",
+        category:"Rose Arrangements",
+        price:"3760"
+    },
+    {
+        id:"8",
+        name:"Meadow Fresh Flower Bouquet",
+        image:"bouquet22.jpg",
+        description:" ",
+        category:"Birthday Collections",
+        price:"3385"
+    }, 
+    {
+        id:"9",
+        name:"Stylish Spring Fresh Bouquet",
+        image:"bouquet23.jpg",
+        description:" ",
+        category:"Birthday Collections",
+        price:"3850"
+    },
+    {
+        id:"10",
+        name:"",
+        image:"bouquet51.jpg",
+        description:"  ",
+        category:"Congraulations Collection",
+        price:"5850"
+    },
+    {
+        id:"11",
+        name:"Sheaf of 9 Geberas",
+        image:"bouquet52.jpg",
+        description:" ",
+        category:"Congraulations Collection",
+        price:"3640"
+    },
+    {
+        id:"12",
+        name:"Thank You Teacher Single Flower Arrangement",
+        image:"bouquet61.jpg",
+        description:"",
+        category:"Thank You Collection",
+        price:"690"
+    },
+    {
+        id:"13",
+        name:"Classic White Rose Arrangement",
+        image:"bouquet71.jpg",
+        description:"",
+        category:"Sympathy Collection",
+        price:"14570"
+    },
+    {
+        id:"14",
+        name:"Golden White Delight Bouquet",
+        image:"bouquet72.jpg",
+        description:"",
+        category:"Sympathy Collection",
+        price:"9770"
+    },
+    {
+        id:"15",
+        name:"Wintry White Posy",
+        image:"bouquet81.jpg",
+        description:". ",
+        category:"Get well Collection",
+        price:"3075"
+    },
+]
