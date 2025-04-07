3fc0968b95e039d75e3fd9b9af273a11d1f672f6
poojak <139669968+pooja180801@users.noreply.github.com>
2025-04-07 09:04:28 +0530

    set the adon default page to be add item page

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
@@ -20,9 +20,10 @@ const App = () => {
       <div className="app-content">
         <Sidebar/>
         <Routes>
+          <Route path='/' element={<Navigate to="/add" />} />
           <Route path='/add' element={<Add url={url}/>}/>
           <Route path='/list' element={<List url={url}/>}/>
-          <Route path='/order' element={<Orders url={url}/>}/>
+          <Route path='/orders' element={<Orders url={url}/>}/>
         </Routes>
       </div>
     </div>
